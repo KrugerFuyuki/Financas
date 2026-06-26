b08e0e469c854430c9f833c26cdf9e349157c682
Fuyuki Krüger <gabriel.kruger03@gmail.com>
2026-06-25 21:56:38 -0300

    primeiras linhas do sistema de financas

---
+++ financas.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+lancamentos_json = 'lancamentos.json'
+
+def carregar_lancamentos():
+    if os.path.exists(lancamentos_json):
+        with open(lancamentos_json, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    return {"lançamentos": []}
+
+def salvar_lancamentos(dados):
+    with open(lancamentos_json, "w", encoding="utf-8") as f:
+        json.dump(dados, f, ensure_ascii=False, indent=4)
+
+receitas = {}
+
+while True:
+    try:
+        print("\nAs opções que podem ser utilizadas são:")
+
+        print("1 - Registrar (receita ou dispesa nova)")
+        print("2 - Ver extrato")
+        print("3 - Relatório de saldo")
+        print("4 - Gerar um relatório.txt")
+        print("5 - Encerrar operação")
+
+        opcao = input("escolha a operação que deseja prosseguir com:")
+
+
+
+
+    except Exception as e:
+        print(f"\nUm erro inesperado ocorreu")
